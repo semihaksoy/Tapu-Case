e985e19983b95d1c6a152f1bb48c8a9abb46a2a2
SEMIH AKSOY <semih.aksoy@sapure.com>
2023-07-04 14:56:59 +0300

    last changes

--- src/components/pages/List.tsx
+++ src/components/pages/List.tsx
@@ -1,9 +1,51 @@
-function List(){
-    return(
-        <div>
-            <h1>List</h1>
-        </div>
-    )
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useDispatch } from 'react-redux'; // <-- import this
+import { addItem } from "../../features/user/userBasketSlice";
+
+export interface Property {
+    name: string;
+    address: string;
+    score: number;
+    distance: number;
+    price: number;
+    photo: string;
 }
+
+const List: React.FC = () => {
+    const [properties, setProperties] = useState<Property[]>([]);
+    const dispatch = useDispatch();
+    async function getData() {
+        try {
+            const response = await axios.get('http://localhost:3000/properties');
+            setProperties(response.data);
+        } catch (error) {
+            console.error("Error fetching data: ", error);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Property List</h1>
+            {properties && <ul>
+                {properties.map((property, index) => (
+                    <li key={index} style={{ marginBottom: '20px' }}>
+                        <h2>{property.name}</h2>
+                        <p>Address: {property.address}</p>
+                        <p>Score: {property.score}/5</p>
+                        <p>Distance: {property.distance}/10</p>
+                        <p>Price: {property.price}</p>
+                        <img src={property.photo} alt={property.name} width="200" />
+                        <button onClick={() => dispatch(addItem(property))}>Add to basket</button>
+                    </li>
+                ))}
+            </ul>}
+        </div>
+    );
+};
 
 export default List;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userAccountReducer from '../features/user/userAccountSlice';
+import basketReducer from '../features/user/userBasketSlice';
 
 const store = configureStore({
   reducer: {
     userAccount: userAccountReducer,
+    basket: basketReducer,
   },
 });
 
